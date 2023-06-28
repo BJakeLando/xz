40314ccb6082a04ae4670d76f6a763bfaf067aa1
Brandon Landers <bjlando@gmail.com>
2023-06-27 17:45:23 -0700

    deploy 9

--- wbk/settings.py
+++ wbk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ac+bc(7^z#$n64*!xxas)mlj!*1e7-e&3w=%l382%tk+vi&x7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
