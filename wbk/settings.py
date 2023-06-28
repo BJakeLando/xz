ba24412be0ac26a52bfb6deeaa8a72e4902e9330
Brandon Landers <bjlando@gmail.com>
2023-06-27 17:44:52 -0700

    deploy 9

--- wbk/settings.py
+++ wbk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ac+bc(7^z#$n64*!xxas)mlj!*1e7-e&3w=%l382%tk+vi&x7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://web-production-51ef.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
